ac65d52a145d452da5aebe366d2c49f02291e28a
Darrel C <dcastellano2007@gmail.com>
2019-08-23 03:43:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         }
      }
 
-		 programmer_hash[alan_kay][known_for]
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -106,6 +106,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for]
 
 
 end
